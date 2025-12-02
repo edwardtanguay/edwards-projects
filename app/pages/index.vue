d67e6335f9259128636ce4fad78c6b94f367c532
Edward Tanguay (windows) <edward@tanguay.info>
2025-12-02 18:57:04 +0100

    FEATURE: Create basic info and screenshots for all start sites

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,64 +13,67 @@ const liveProjects = _projects.filter((project) =>
 const showcaseProjects = _projects.filter((project) =>
   project.categories.some((category) => category.id_code === "showcase")
 );
+const startProjects = _projects.filter((project) =>
+  project.categories.some((category) => category.id_code === "start")
+);
 </script>
 
 <template>
   <div class="flex flex-col gap-2 mt-4">
     <div v-if="activeProjects.length">
       <h2 class="text-2xl font-bold mb-3">Active Projects</h2>
-      <DisplayProject
-        v-for="project in activeProjects"
-        :key="project.suuid"
-        :category="`active`"
-        :project="project"
-        :short_info="
-          project.categories.find((category) => category.id_code === 'active').short_info
-        "
-      />
+      <DisplayProject v-for="project in activeProjects"
+                      :key="project.suuid"
+                      :category="`active`"
+                      :project="project"
+                      :short_info="project.categories.find((category) => category.id_code === 'active').short_info
+                        " />
     </div>
 
     <div v-if="upcomingProjects.length">
       <h2 class="text-2xl font-bold mb-3">Upcoming Projects</h2>
-      <DisplayProject
-        v-for="project in upcomingProjects"
-        :key="project.suuid"
-        :category="`upcoming`"
-        :project="project"
-        :short_info="
-          project.categories.find((category) => category.id_code === 'upcoming')
-            .short_info
-        "
-      />
+      <DisplayProject v-for="project in upcomingProjects"
+                      :key="project.suuid"
+                      :category="`upcoming`"
+                      :project="project"
+                      :short_info="project.categories.find((category) => category.id_code === 'upcoming')
+                          .short_info
+                        " />
     </div>
 
     <div v-if="liveProjects.length">
       <h2 class="text-2xl font-bold mb-3">Live and in Production</h2>
-      <DisplayProject
-        v-for="project in liveProjects"
-        :key="project.suuid"
-        :title="``"
-        :category="`live`"
-        :project="project"
-        :short_info="
-          project.categories.find((category) => category.id_code === 'live').short_info
-        "
-      />
+      <DisplayProject v-for="project in liveProjects"
+                      :key="project.suuid"
+                      :title="``"
+                      :category="`live`"
+                      :project="project"
+                      :short_info="project.categories.find((category) => category.id_code === 'live').short_info
+                        " />
     </div>
 
     <div v-if="showcaseProjects.length">
       <h2 class="text-2xl font-bold mb-3">Showcases</h2>
-      <DisplayProject
-        v-for="project in showcaseProjects"
-        :key="project.suuid"
-        :title="``"
-        :category="`showcase`"
-        :project="project"
-        :short_info="
-          project.categories.find((category) => category.id_code === 'showcase')
-            .short_info
-        "
-      />
+      <DisplayProject v-for="project in showcaseProjects"
+                      :key="project.suuid"
+                      :title="``"
+                      :category="`showcase`"
+                      :project="project"
+                      :short_info="project.categories.find((category) => category.id_code === 'showcase')
+                          .short_info
+                        " />
+    </div>
+
+    <div v-if="startProjects.length">
+      <h2 class="text-2xl font-bold mb-3">Starts</h2>
+      <DisplayProject v-for="project in startProjects"
+                      :key="project.suuid"
+                      :title="``"
+                      :category="`start`"
+                      :project="project"
+                      :short_info="project.categories.find((category) => category.id_code === 'start')
+                          .short_info
+                        " />
     </div>
   </div>
 </template>
